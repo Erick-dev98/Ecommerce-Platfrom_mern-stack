9a9dc2df01aa25a8ba029dd8cb669b81f31c7c8d
erickmwei680@gmail.com <erickmwei680@gmail.com>
2024-03-23 18:35:40 +0300

    first commit

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -3,10 +3,14 @@ import "./Profile.scss";
 import PageMenu from '../../components/pageMenu/PageMenu';
 import { useDispatch, useSelector } from 'react-redux';
 import Card from '../../components/card/Card';
-import { getUser, updateUser } from '../../redux/features/auth/authSlice';
+import { getUser, updatePhoto, updateUser } from '../../redux/features/auth/authSlice';
 import Loader from '../../components/loader/Loader';
 import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { toast } from 'react-toastify';
 
+const cloud_name = "dqyhngi72";
+const upload_preset = "vnzeoapc";
+const url = "https://api.cloudinary.com/v1_1/dqyhngi72/image/upload";
 
 const Profile = () => {
     const {isLoading, user } = useSelector((state) => state.auth);
@@ -75,7 +79,31 @@ const Profile = () => {
 
         await dispatch(updateUser(userData));
     };
-    const savePhoto = async () => {};
+    const savePhoto = async (e) => {
+        e.preventDefault()
+        let imageURL;
+
+        try {
+            if (
+                profileImage !== null &&
+                (profileImage.type === "image/jpeg" || profileImage.type ===
+                "image/jpg" || profileImage.type === "image/png")
+            ) {
+                const image = new FormData()
+                image.append("file", profileImage)
+                image.append("cloud_name", cloud_name)
+                image.append("upload_preset", upload_preset)
+
+                // Save image to cloudinary
+                const response = await fetch(url, {method: "post", body:image})
+                const imgData = await response.json()
+                console.log(imgData);
+            }           
+            
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
 
   return (
     <>
